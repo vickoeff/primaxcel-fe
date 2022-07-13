00aedc5042577a177e18b7782955c2009ac6c9e2
Vicko <vicko.effendy@swiftsolutions.id>
2022-07-13 07:55:29 +0700

    create simple axios hook

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,44 @@
-import { Box, Button, Table, Tr, Th, Td } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Box, Button, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import { ContainerSection } from "../components/Container";
+import { callApi } from "../api/useApiHook";
 
 export default function Home() {
+  const [random, setRandom] = useState(null);
+
+  useEffect(() => {
+    getRandomData();
+  }, []);
+
+  const getRandomData = () => {
+    callApi("get", "/random")
+      .then((res) => setRandom(res))
+      .catch((err) => alert(err.message));
+  };
+
   return (
     <ContainerSection>
       <Box>
-        <Button colorScheme="blue">Test fetch</Button>
+        <Button colorScheme="blue" onClick={() => getRandomData()}>
+          Refetch
+        </Button>
         <Table mt={6}>
-          <Tr>
-            <Th>No.</Th>
-            <Th>Tittle</Th>
-            <Th>Content</Th>
-          </Tr>
-          <Tr>
-            <Td>1</Td>
-            <Td>Get Content API</Td>
-            <Td>
-              1. Product A<br />
-              2. Product B
-            </Td>
-          </Tr>
+          <Thead>
+            <Tr>
+              <Th>API</Th>
+              <Th>Category</Th>
+              <Th>Description</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {random?.entries?.map((entry, key) => (
+              <Tr key={key}>
+                <Td>{entry.API}</Td>
+                <Td>{entry.Category}</Td>
+                <Td>{entry.Description}</Td>
+              </Tr>
+            ))}
+          </Tbody>
         </Table>
       </Box>
     </ContainerSection>
